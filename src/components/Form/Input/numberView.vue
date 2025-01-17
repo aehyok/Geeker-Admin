ffad3ebe1c8d3125e989d86f91f3b7ccfb343cbd
刘启明 <455043818@qq.com>
2025-01-17 18:59:31 +0800

    feat: number 类型初始化待修改为ts

--- src/components/Form/Input/numberView.vue
+++ src/components/Form/Input/numberView.vue
@@ -1,6 +1,95 @@
+<!--number数值类型-->
 <template>
-  <div>numberView</div>
+  <el-form-item v-model:label="column.label" :prop="column.name" :rules="rules">
+    <el-input
+      type="number"
+      v-model.number="value"
+      :controls="controls"
+      :precision="column.precision"
+      :style="{ width: column.width || '100%', 'text-align': 'left' }"
+      :placeholder="placeholder"
+      :readonly="props.column.readonly"
+      :disabled="props.column.disabled || props.column.readonly"
+      :size="props.column.size"
+      @input="inputFun"
+      :max="column.max"
+      :min="column.min"
+    >
+      <template #suffix v-if="column.append">
+        <div class="append-class">
+          <span>{{ column.append }}</span>
+        </div>
+      </template>
+    </el-input>
+    <div v-if="column.remark">
+      {{ column.remark }}
+    </div>
+  </el-form-item>
 </template>
-<script setup lang="ts">
-console.log("numberView");
+<script setup>
+import { computed, watch, ref } from "vue";
+
+const emit = defineEmits(["update:data"]);
+const props = defineProps({
+  column: {
+    type: [Object]
+  },
+  formData: {
+    type: [Object]
+  },
+  data: {
+    type: [Number, String, Boolean],
+    default: undefined
+  }
+});
+
+const { column } = props;
+const rules = ref([
+  {
+    required: !!column.required,
+    message: `请输入${column.label}`,
+    trigger: "blur"
+  }
+]);
+
+const inputFun = e => {
+  props.column.inputFunction && props.column.inputFunction(e);
+};
+
+const controls = column.controls ?? false;
+const placeholder = column.placeholder ? column.placeholder : `请输入${column.label}`;
+const value = computed({
+  get: function () {
+    return column.precision ? typeof props.data == "number" && parseFloat(props.data?.toFixed(column.precision)) : props.data;
+  },
+  set: function (val) {
+    if (column.precision || column.precision === 0) {
+      emit("update:data", val ? parseFloat(val.toFixed(column.precision)) : 0);
+    } else {
+      emit("update:data", val);
+    }
+  }
+});
+
+watch(
+  () => props.data,
+  () => {
+    if (column.changefunction) {
+      try {
+        const evalFunction = eval(column.changefunction);
+        evalFunction(props.formData, column);
+      } catch (e) {
+        console.log("changefunction number error!!!", e);
+      }
+    }
+  },
+  {
+    immediate: true
+  }
+);
 </script>
+<style lang="scss" scoped>
+:deep(.el-input-number.is-without-controls .el-input__inner) {
+  text-align: left;
+}
+</style>
--- src/components/Form/Input/textareaView.vue
+++ src/components/Form/Input/textareaView.vue
@@ -1,61 +1,108 @@
-<!--简单文本框-->
+<!--number数值类型-->
 <template>
-  <el-form-item :label="column.label" :prop="column.name" :rules="rules">
+  <el-form-item v-model:label="column.label" :prop="column.name" :rules="rules">
     <el-input
-      v-model="value"
-      type="textarea"
+      type="number"
+      v-model.number="value"
+      :controls="controls"
+      :precision="column.precision"
+      :style="{ width: column.width || '100%', 'text-align': 'left' }"
       :placeholder="placeholder"
-      :rows="column.rows"
-      :maxlength="column.maxlength"
-      :minlength="column.minlength"
-      :show-word-limit="column.showwordlimit"
-      :autosize="column.autosize"
-      :disabled="!!props.column.disabled"
-    />
+      :readonly="props.column.readonly"
+      :disabled="props.column.disabled || props.column.readonly"
+      :size="props.column.size"
+      @input="inputFun"
+      :max="column.max"
+      :min="column.min"
+    >
+      <template #suffix v-if="column.append">
+        <div class="append-class">
+          <span>{{ column.append }}</span>
+        </div>
+      </template>
+    </el-input>
+    <div v-if="column.remark">
+      {{ column.remark }}
+    </div>
   </el-form-item>
 </template>
-
-<script lang="ts" setup name="TextAreaView">
-import { computed, ref, toRefs } from "vue";
-import { FormModule } from "@/typings/form";
+<script setup>
+import { computed, watch, ref } from "vue";
+import { formUtils } from "../utils/index.ts";
+const { initializeRules } = formUtils();
 
 const emit = defineEmits(["update:data"]);
-interface TextAreaProps {
-  column: FormModule.FormTextAreaColumn;
-  formData: any;
-  data?: string;
-}
-
-const props = withDefaults(defineProps<TextAreaProps>(), {
-  data: undefined
+const props = defineProps({
+  column: {
+    type: [Object],
+    default: () => {},
+  },
+  formData: {
+    type: [Object],
+    default: () => {},
+  },
+  data: {
+    type: [Number, String, Boolean],
+    default: undefined,
+  },
 });
 
-const { column } = toRefs(props);
-if (column.value && !column.value.rows) {
-  column.value.rows = 3;
-}
-
-const rules = ref<FormModule.FormColumnRule[]>([]);
-const placeholder = column.value.placeholder ? column.value.placeholder : `请输入${column.value.label}`;
-
-rules.value = [
+const { column } = props;
+const rules = ref([
   {
-    required: column.value.required,
-    message: placeholder,
-    trigger: ["blur", "change"]
-  }
-];
+    required: !!column.required,
+    message: `请输入${column.label}`,
+    trigger: "blur",
+  },
+]);
 
-if (column.value && column.value.rules) {
-  rules.value = [...rules.value, ...column.value.rules];
+if (column && column.rules && Array.isArray(column.rules)) {
+  rules.value = [...rules.value, ...initializeRules(column.rules, props)];
 }
 
+const inputFun = (e) => {
+  props.column.inputFunction && props.column.inputFunction(e);
+};
+
+const controls = column.controls ?? false;
+const placeholder = column.placeholder
+  ? column.placeholder
+  : `请输入${column.label}`;
 const value = computed({
   get: function () {
-    return props.data;
+    return column.precision
+      ? typeof props.data == "number" &&
+          parseFloat(props.data?.toFixed(column.precision))
+      : props.data;
   },
   set: function (val) {
-    emit("update:data", val);
-  }
+    if (column.precision || column.precision === 0) {
+      emit("update:data", val ? parseFloat(val.toFixed(column.precision)) : 0);
+    } else {
+      emit("update:data", val);
+    }
+  },
 });
+
+watch(
+  () => props.data,
+  (newValue) => {
+    if (column.changefunction) {
+      try {
+        const evalFunction = eval(column.changefunction);
+        evalFunction(props.formData, column);
+      } catch (e) {
+        console.log("changefunction number error!!!", e);
+      }
+    }
+  },
+  {
+    immediate: true,
+  }
+);
 </script>
+<style lang="scss" scoped>
+:deep(.el-input-number.is-without-controls .el-input__inner) {
+  text-align: left;
+}
+</style>
